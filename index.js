9a25d486fea03819bcc35ffb5b240fb6e302e8e6
Adrian Jones <adrianjones@duck.com>
2023-07-21 10:19:49 -0700

    fix: unify drawing pad
    The drawing pad used to be split into seperate rows, with large gaps between them. This issue has been mostly fixed, so the drawing pad is (for the most part) now a unified whole

--- index.js
+++ index.js
@@ -1,55 +1,36 @@
 // defining the DOM node
 const theDiv = document.querySelector("#theDiv");
+
+const theGrid = document.createElement("div");
 theDiv.style.margin = "auto";
-theDiv.style.marginTop = "80px";
-theDiv.style.width = "660px";
-theDiv.style["grid-template-rows"] = "repeat(100, 1fr)";
+theDiv.style.display = "grid";
+theDiv.style.width = "460px";
 theDiv.style["grid-template-columns"] = "repeat(100, 1fr)";
-theDiv.style.justifyItems = "center";
+theDiv.style["grid-column-gap"] = "0";
+theDiv.style["grid-row-gap"] = "0";
+theDiv.style.rowGap = "0";
+theDiv.style.columnGap = "0";
+theDiv.style["grid-column-gap"] = "0";
 
 function changeColor(buttonClass, color) {
   buttonClass.style.backgroundColor = color;
   return;
 }
 
-
-for (let i = 0; i < 42; i++) {
+for (let i = 0; i < 4200; i++) {
   const pixel = document.createElement("div");
-  console.log(pixel);
   pixel.classList.add("pixel");
-  pixel.style.height = "100px";
-  pixel.style.width = "100px";
+  pixel.style.height = "5px";
+  pixel.style.width = "5px";
   pixel.style.backgroundColor = "grey";
-  console.log("ffs");
+  pixel.style.margin = "0";
   theDiv.appendChild(pixel);
 }
 
-/*
-function makeRows(rows, cols) {
-  theDiv.style.setProperty('--grid-rows', rows);
-  theDiv.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows); c++) {
-    let row = document.createElement("div");
-    row.display = "in-line grid";
-    // row.display["grid-template-columns"] = `repeat(${cols}, 1fr)`;
-      for (i = 0; i < cols; i++) {
-        let pixel= document.createElement("div");
-        pixel.innerText = (c + 1);
-        pixel.classList.add('pixel');
-        row.appendChild(pixel);
-      }
-    theDiv.appendChild(row).className = "grid-item";
-  };
-};
-*/
-// drawPixels()
-//makeRows(16, 16);
-// defining the size for each button at once
 const pixels = document.querySelectorAll(".pixel")
 pixels.forEach((pixel) => {
   console.log("hello?");
   pixel.addEventListener("mouseenter", () => {
-    console.log("help");
     pixel.style.backgroundColor = "black";
     changeColor(pixel, "black");  
   });
@@ -63,6 +44,6 @@ theDiv.appendChild(clear);
 
 clear.addEventListener("click", () => {
   pixels.forEach((pixel) => {
-    changeColor(pixel, "white");
+    changeColor(pixel, "grey");
   });
 });
